8e6eb32684b3c457e609dc0da00d5f353ae8ec12
Inshaal Uddin <inshaal@users.noreply.github.com>
2017-04-01 10:19:31 +0530

    code changes

--- inventory-search.py
+++ inventory-search.py
@@ -1,22 +1,21 @@
-#Inventory.Updates
+#Making an inventory & searching in it.
 d={}
 while True:
     a=raw_input("Enter the product name : ")
-    b=raw_input("Enter the product price : ")
+    b=raw_input("Enter the price of that product : ")
     d[a]=b
     c=raw_input("Do you want to add more products? [Y/N] ")
     if c=='Y' or c=='y':
         continue
     else:
         break
-print "The inventory has been created!"
+print "The inventory has been created!"  #just to inform that the first part of our program has finished working.
 while True:
     x=raw_input("Enter the product name you want to search for : ")
     print "The price of that product is : ",d[x]
-    y=raw_input("Do you want to search price of more products? [Y/N] ")
+    y=raw_input("Do you want to search the price of more products? [Y/N] ")
     if y=='Y' or y=='y':
         continue
     else:
         break
 print "Program stopped. "
-print "done."
